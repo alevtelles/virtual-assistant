b7060c4d008530be9d7ce705e9f62922ed95ca42
Valente Alexsander <alevtelles@gmail.com>
2025-04-26 19:38:01 -0300

    feat: add docker

--- core/settings.py
+++ core/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'NAME': os.getenv('POSTGRES_DB', 'virtual_assistant'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('POSTGRES_HOST', 'postgres'),  # 'postgres' é o nome do serviço no docker-compose
         'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
